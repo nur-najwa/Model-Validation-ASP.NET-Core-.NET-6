27b7e681d3c94f28b001b9f2f8520033638a6113
Najwa <najwa@iris.com.my>
2023-02-27 10:15:40 +0800

    first commit

---
+++ ModelValidationAPI/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelValidationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult <User> Post([FromBody] User user)
+        {
+            //Some Operations
+            return Ok(user);
+        }
+
+    }
+}
---
+++ ModelValidationAPI/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationAPI.Models
+{
+    public class User
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$")]
+        public string Phone { get; set; }
+
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$")]
+        public string Password { get; set; }    
+    }
+}
